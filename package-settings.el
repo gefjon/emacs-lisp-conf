89abab4ddd25b861bd66456eb1e735b06d69761c
Arthur Goldman <arthur@goldman-tribe.org>
2017-11-29 15:58:16 +0100

    Delete package-settings.el

--- package-settings.el
+++
@@ -1,8 +0,0 @@
-(package-initialize)
-
-(setq package-archives '(("elpa" . "https://elpa.gnu.org/packages/")
-			 ("marmalade" . "https://marmalade-repo.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")))
-(setf package-selected-packages (nconc package-selected-packages '(column-marker auto-complete racer rust-playground glsl-mode pretty-symbols nginx-mode json-mode pug-mode go-complete go-mode toml-mode flymd markdown-mode markdown-mode+ exec-path-from-shell cargo elpy flycheck flycheck-rust js2-mode rust-mode)))
-
-(provide 'package-settings)
