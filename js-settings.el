56ebc7ef7572105840a834aae8faf607d99863d8
Arthur Goldman <arthur@goldman-tribe.org>
2017-12-31 11:21:22 +0100

    removed more :ensure t s for always-ensure cleanness

--- js-settings.el
+++ js-settings.el
@@ -2,24 +2,20 @@
   :mode "\\.js\\'"
   :interpreter "node"
   :config (add-hook 'js2-mode-hook
-	  (lambda ()
-	    (flycheck-mode t)
-	    (flycheck-select-checker 'javascript-standard)
-	    (setf js2-mode-show-parse-errors nil)
-	    (setf js2-mode-show-strict-warnings nil)
-	    (setf js2-basic-offset 2)))
-  :ensure t)
+	  ;; (lambda ()
+	  ;;   (flycheck-mode t)
+	  ;;   (flycheck-select-checker 'javascript-standard)
+	  ;;   (setf js2-mode-show-parse-errors nil)
+	  ;;   (setf js2-mode-show-strict-warnings nil)
+	  ;;   (setf js2-basic-offset 2))
+          ))
 (use-package json-mode
-  :mode "\\.json\\'"
-  :ensure t)
+  :mode "\\.json\\'")
 (use-package pug-mode
-  :mode "\\.pug\\'"
-  :ensure t)
+  :mode "\\.pug\\'")
 (use-package sass-mode
-  :ensure t
   :mode "\\.sass\\'")
 (use-package typescript-mode
-  :ensure t
   :mode "\\.ts\\'")
 
 (provide 'js-settings)
