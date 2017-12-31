12de247b1cabf535ef0b520ebfc5068ae43b6b0c
Arthur Goldman <arthur@goldman-tribe.org>
2017-12-31 11:22:47 +0100

    fixed a comment error

--- js-settings.el
+++ js-settings.el
@@ -1,14 +1,14 @@
 (use-package js2-mode
   :mode "\\.js\\'"
   :interpreter "node"
-  :config (add-hook 'js2-mode-hook
+  ;; :config (add-hook 'js2-mode-hook
 	  ;; (lambda ()
 	  ;;   (flycheck-mode t)
 	  ;;   (flycheck-select-checker 'javascript-standard)
 	  ;;   (setf js2-mode-show-parse-errors nil)
 	  ;;   (setf js2-mode-show-strict-warnings nil)
 	  ;;   (setf js2-basic-offset 2))
-          ))
+          )
 (use-package json-mode
   :mode "\\.json\\'")
 (use-package pug-mode
