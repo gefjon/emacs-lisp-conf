6c02106db0d2c07ac415717b2ba0f348dfe7f898
Phoebe Goldman <phoebe@goldman-tribe.org>
2021-01-07 13:05:09 -0600

    org-roam!

--- markup-settings.el
+++ markup-settings.el
@@ -6,8 +6,7 @@
 (defun markup-editing-settings ()
   (set-fill-column 90)
   (pandoc-mode 1)
-  (pandoc-load-default-settings)
-  (flyspell-mode))
+  (pandoc-load-default-settings))
 
 (use-package fountain-mode
   :mode "\\.fountain\\'"
@@ -17,11 +16,13 @@
   :mode "\\.md\\'"
   :hook (markdown-mode . markup-editing-settings))
 
+(defvar org-notes-dir (expand-file-name "~/notes"))
+
 (use-package org
   :config
   (setf org-src-fontify-natively t
         org-log-done 'time
-        org-agenda-files (list (expand-file-name "~/sift"))
+        org-agenda-files (list org-notes-dir)
         org-agenda-start-day "-1d"
         org-agenda-span 16
         org-image-actual-width nil)
@@ -31,5 +32,14 @@
   :bind (("C-c a" . org-agenda)
          ("C-c l" . org-store-link))
   :hook (org-mode . markup-editing-settings))
+
+(use-package org-roam
+  :config
+  (setf org-roam-directory org-notes-dir)
+  (org-roam-mode)
+  :bind (("C-c f" . org-roam-find-file)
+         ("C-c r" . org-roam)
+         :map org-mode-map
+         ("C-c i" . org-roam-insert)))
 
 (provide 'markup-settings)
