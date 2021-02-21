f042de6aa365fa3e5b3a48df0b6bfcf4b8a056e6
Phoebe Goldman <phoebe@goldman-tribe.org>
2021-02-21 13:28:27 -0500

    sbcl arm64 version!

--- lisp-config.el
+++ lisp-config.el
@@ -7,15 +7,19 @@
   :config
   (setf sly-lisp-implementations
         '((sbcl-fast ("sbcl" "--core" "/Users/phoebe/sbcl.core-with-slynk"))
+          (sbcl-arm64 ("arch" "-arm64" "sbcl"))
           (sbcl ("sbcl"))
           (allegro ("alisp"))
           (ccl ("ccl")))
-        sly-default-lisp 'sbcl)
+        sly-default-lisp 'sbcl-arm64)
   (add-hook 'sly-mrepl-mode-hook #'smartparens-mode)
   (cl-flet ((start-sly ()
                        (unless (sly-connected-p)
-                         (save-excursion (sly)))))
-    (add-hook 'lisp-mode-hook #'start-sly))
+                         (save-excursion (sly))))
+            (fill-110 ()
+                      (set-fill-column 110)))
+    (add-hook 'lisp-mode-hook #'start-sly)
+    (add-hook 'lisp-mode-hook #'fill-110))
   :bind (:map sly-mode-map ))
 
 (provide 'lisp-config)
