e1020d2352fc792e726683f38f298309c8c7f386
Phoebe Goldman <phoebe@goldman-tribe.org>
2021-09-17 12:49:51 -0400

    update org-roam; defhydra for org stuff; fixed-width inline images

--- markup-settings.el
+++ markup-settings.el
@@ -25,7 +25,7 @@
         org-agenda-files (list org-notes-dir)
         org-agenda-start-day "-1d"
         org-agenda-span 16
-        org-image-actual-width nil)
+        org-image-actual-width 500)
   (org-babel-do-load-languages 'org-babel-load-languages
                                '((emacs-lisp . t)
                                  (lisp . t)))
@@ -35,11 +35,19 @@
 
 (use-package org-roam
   :config
-  (setf org-roam-directory org-notes-dir)
-  (org-roam-mode)
-  :bind (("C-c f" . org-roam-find-file)
+  (setf org-roam-directory org-notes-dir
+        org-roam-v2-ack t)
+  (org-roam-setup)
+  :bind (("C-c f" . org-roam-node-find)
          :map org-mode-map
          ("C-c r" . org-roam)
          ("C-c i" . org-roam-insert)))
+
+(use-package hydra)
+(defhydra hydra-org-dispatch (org-mode-map "C-c o"
+                                           ;; i.e :foreign-keys warn :exit t
+                                           :color teal)
+  "Catch-all dispatch for org-mode related stuff."
+  ("f" org-footnote-new))
 
 (provide 'markup-settings)
